7f0f37862542b716a6795b4f8cf1cdd2268f6b86
Revelax <148916670+Revelax@users.noreply.github.com>
2024-01-10 16:23:10 +0300

    UserServiceImp: getUserCar: указал только чтение для транзакции

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -26,7 +26,7 @@ public class UserServiceImp implements UserService {
       return userDao.listUsers();
    }
 
-   @Transactional
+   @Transactional(readOnly = true)
    @Override
    public List<User> getUserCar(String model, int series) {
       return userDao.getUserCar(model, series);
